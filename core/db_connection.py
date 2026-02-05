7348ad970ae093ddfb55e7a1eff86d92a037dc48
AR Rahman Iftekhar Shovon <ar.rahmaniftekharshovon21@gmail.com>
2026-02-06 00:19:39 +0600

    Add secure config management

--- core/db_connection.py
+++ core/db_connection.py
@@ -7,12 +7,15 @@ def create_connection():
     Returns connection object if successful, None otherwise
     """
     try:
-        connection = mysql.connector.connect(
-            host='localhost',
-            database='smart_hostel_db',
-            user='',        #TODO: Change this to your MySQL username
-            password=''     #TODO: Change this to your MySQL password
-        )
+        try:
+            from core.config import DB_CONFIG
+            connection = mysql.connector.connect(**DB_CONFIG)
+        except ImportError:
+            # If config.py doesn't exist, use manual input
+            print("config.py not found!")
+            print("Please create core/config.py with your database credentials")
+            print("See README.md for instructions")
+            return None
         
         if connection.is_connected():
             print("Successfully connected to MySQL database")
